f114a52dc05c5b4befc08a5a498865f3c403ff5f
Koen Beckers <koen@k-4u.nl>
2023-08-13 10:45:19 +0200

    Removed all references to instance name.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 GAME_DIR="/appdata/space-engineers/SpaceEngineersDedicated"
 INSTANCES_DIR="/appdata/space-engineers/instances"
 PLUGIN_DIR="/appdata/space-engineers/plugins"
-CONFIG_PATH="${INSTANCES_DIR}/${INSTANCE_NAME}/SpaceEngineers-Dedicated.cfg"
+CONFIG_PATH="${INSTANCES_DIR}/SpaceEngineers-Dedicated.cfg"
 INSTANCE_IP=$(hostname -I | sed "s= ==g")
 
 
@@ -17,7 +17,7 @@ sed -i "s=<IP>.*</IP>=<IP>${INSTANCE_IP}</IP>=g" ${CONFIG_PATH}
 
 # update world save path
 CURRENT_WORLDNAME=$(grep -oEi '<WorldName>(.*)</WorldName>' ${CONFIG_PATH} | sed -E "s=<WorldName>|</WorldName>==g")
-SAVE_PATH="Z:\\\\appdata\\\\space-engineers\\\\instances\\\\${INSTANCE_NAME}\\\\Saves\\\\${CURRENT_WORLDNAME}";
+SAVE_PATH="Z:\\\\appdata\\\\space-engineers\\\\instances\\\\Saves\\\\${CURRENT_WORLDNAME}";
 sed -E -i "s=<LoadWorld />|<LoadWorld.*LoadWorld>=<LoadWorld>${SAVE_PATH}</LoadWorld>=g" ${CONFIG_PATH}
 
 echo "---------------------------------UPDATE PLUGINS------------------------------"
@@ -44,9 +44,9 @@ echo "SAVE_PATH=$SAVE_PATH"
 ## END UPDATES ##
 wine --version
 echo "----------------------------------START GAME---------------------------------"
-rm ${INSTANCES_DIR}/${INSTANCE_NAME}/*.log
+rm ${INSTANCES_DIR}/*.log
 cd ${GAME_DIR}/DedicatedServer64/
-wine SpaceEngineersDedicated.exe -noconsole -ignorelastsession -path Z:\\appdata\\space-engineers\\instances\\${INSTANCE_NAME}
+wine SpaceEngineersDedicated.exe -noconsole -ignorelastsession -path Z:\\appdata\\space-engineers\\instances
 echo "-----------------------------------END GAME----------------------------------"
 sleep 1
 echo "-----------------------------------BYE !!!!----------------------------------"
